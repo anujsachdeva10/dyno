ee0fea9ad42e3af6657d78d77f08da3400588621
anujsachdeva10 <anujsachdeva10@gmail.com>
2021-02-05 16:05:57 +0530

    deployment ready project

--- data/settings.py
+++ data/settings.py
@@ -28,9 +28,9 @@ TEMPLATE_DIR = BASE_DIR / 'dproject/templates/dproject'
 SECRET_KEY = 'bv9ndd!zh7h#_g)y*!r%rr#hhwrov=t1u0)l^o3b13_49)5v6s'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
--- data/urls.py
+++ data/urls.py
@@ -14,12 +14,19 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from dproject import views
+from django.conf import settings
+from django.conf.urls.static import static
+
+from django.views.static import serve
+from django.conf.urls import url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.authenticate_user, name='authenticate'),
     path('insertdata/', views.insert_credentials, name='insert_credentials'),
-    path('logout/', views.user_logout, name="logout")
+    path('logout/', views.user_logout, name="logout"),
+    url(r'^media/(?P<path>.*)$', serve, ('document_root': settings.MEDIA_ROOT)),
+    url(r'^static/(?P<path>.*)$', serve, ('document_root': settings.STATIC_ROOT)),
 ]
